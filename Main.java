59b254fd0073dd7f25986d5c7452444e0107744c
RishithaBhattarai <69611262+RishithaBhattarai@users.noreply.github.com>
2020-08-12 18:31:34 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Dance");
+        System.out.println("UCLA");
+        System.out.println("Call of Duty");
+        System.out.println("Psychology");
+        System.out.println("Pediatrician");
+	}
+    }
+
+       
